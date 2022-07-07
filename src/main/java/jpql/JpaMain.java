891dce80e36beb56391b5c69376c8943aba1bcc5
yongjo <yjlee90@suresofttech.com>
2022-07-07 18:34:39 +0900

    객체지향 쿼리 언어2 - 중급 문법-경로 표현식

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -1,6 +1,7 @@
 package jpql;
 
 import javax.persistence.*;
+import java.util.Collection;
 import java.util.List;
 
 public class JpaMain {
@@ -14,26 +15,28 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            em.persist(team);
+
             Member member1 = new Member();
             member1.setUsername("관리자1");
+            member1.setTeam(team);
             em.persist(member1);
 
             Member member2 = new Member();
             member2.setUsername("관리자2");
+            member2.setTeam(team);
             em.persist(member2);
-
 
             em.flush();
             em.clear();
 
-            String query = "select group_concat(m.username) From Member m";
+            String query = "select m From Team t join t.members m";
 
-            List<String> result = em.createQuery(query, String.class)
+            List<Collection> result = em.createQuery(query, Collection.class)
                     .getResultList();
 
-            for (String s : result) {
-                System.out.println("s = " + s);
-            }
+            System.out.println("result = " + result);
 
             tx.commit();
         } catch (Exception e) {
