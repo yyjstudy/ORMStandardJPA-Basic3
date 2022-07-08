9aedf07cf9a63fbbc5cb4fa1af52240b12adfdb4
yongjo <yjlee90@suresofttech.com>
2022-07-08 18:18:34 +0900

    객체지향 쿼리 언어2 - 중급 문법-페치 조인 2 - 한계

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -41,9 +41,11 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t From Team t join fetch t.members m";
+            String query = "select t From Team t";
 
             List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
                     .getResultList();
 
             System.out.println("result = " + result.size());
